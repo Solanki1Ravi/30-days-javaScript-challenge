cb11aa897feb6afdd7b3931226700c517b3526bd
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-08-17 20:42:30 +0530

    day1 of 30days javascript challenge
    day1 of 30days javascript challenge

---
+++ 01-Day/task1.js
@@ -0,0 +1,41 @@
+
+//task1
+var myVar = 30;
+// console.log(myVar);
+myVar = 8000;
+console.log(myVar);
+console.log(typeof myVar);
+
+
+//task2
+let myLet = "Ravindar Singh";
+// console.log(myLet);
+console.log(typeof myLet);
+
+
+
+//task3
+const myConst = true;
+// console.log(myConst);
+console.log(typeof myConst);
+
+
+
+//task4
+let myLet1 = 30;
+//let myLet1 = 80; //returns error
+myLet1= "Ravindar Singh";
+// console.log(myLet1); //Easily execute with the last updated value
+console.log(typeof myLet1);
+
+
+
+//task5
+const myConst1 = 300;
+//myConst1 = 4000;
+// console.log(myConst1); //gives "Assignment to constant vairable error"
+console.log(typeof myConst1);
+
+
+
+
