fc19f6ca06c1ec621f9734527aa9203e2b283180
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-08-18 13:25:24 +0530

    Day 2
    Day 2 of 30 days JavaScript challenge

---
+++ Day-02/01-index.js
@@ -0,0 +1,107 @@
+
+//Arithmatic Operations
+
+//task1
+let num1 = 11;
+let num2 = 89;
+
+// let res = num1+num2
+// console.log("Result: ",res);
+
+
+//task2 
+
+let subRes = num1-num2
+// console.log("Subtraction Result: ",subRes);
+
+//task3 
+
+let mulRes = num1*num2
+// console.log("Multiplication Result: ",mulRes);
+
+//task4
+let divRes = num2/num1
+// console.log("Divide Result: ",divRes);
+
+
+//task5
+
+let modRes = num2%num1
+// console.log("Reminder: ",modRes);
+
+
+//Assignment Operator
+
+//taks6
+let num = 300;
+let num3 = 500
+
+// let result = (num+=num3)
+// console.log(`(${num} += ${num3}):=`,(num+=num3));
+
+//task7
+// console.log(`(${num} -= ${num3}):=`,(num-=num3));
+
+
+
+//Comparison Operator
+
+//task8
+// let num11 = 11;
+// let num22 = 89;
+// console.log("Result ",(num11>num22));
+// console.log("Result ",(num11<num22));
+
+//task9
+// console.log("Result ",(num11<=num22));
+// console.log("Result ",(num11>=num22));
+
+//task10
+let num11 = 11;
+let num22 = 89;
+// console.log("Result ",(num11==num22));
+// console.log("Result ",(num11===num22));
+
+
+
+//Logical Operators
+
+//task11
+
+
+let res1 = (num22>num11)
+// let res2 = (num<num3)   //true
+let res2 = (num>num3)     //false
+console.log(`and operator(&&)`,res1&&res2);
+
+//task12
+// console.log(`||(or operator)`,res1||res2);
+
+//task13
+// console.log(`!(not operator)`,!(res1||res2));
+
+
+
+//Ternary Opeator
+
+//task14
+
+let a = -10
+console.log((a>=0)?"positive":"Negative")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day-02/02-Req.js
@@ -0,0 +1,45 @@
+//Arithamtic Operator Script
+
+let num1 = 11;
+let num2 = 89;
+
+console.log("Addition =>",(num1+num2));
+console.log("Subtraction(-) =>",(num1-num2));
+console.log("Multiplication(*) =>",(num1*num2));
+console.log("Divisino =>",(num1/num2));
+console.log("Modulo =>",(num1%num2));
+
+
+//Comparison and Logical Operator Script
+/*
+
+//Comparison Operator
+let num1 = 11;
+let num2 = 89;
+
+console.log("Greater than =>",(num1<num2));
+console.log("Greater than =>",(num1<=num2));
+console.log("Greater than =>",(num1>num2));
+console.log("Greater than =>",(num1>=num2));
+console.log("Greater than =>",(num1==num2));
+console.log("Greater than =>",(num1===num2));
+*/
+/*
+
+//Logical Operator
+let num11 = 11;
+let num21 = 89;
+
+console.log("&& =>",(num1<num2)&&(num11>num21));
+console.log("|| =>",(num1<num2)||(num11>num21));
+console.log("! =>",!(num1<num2)&&(num11>num21));
+*/
+
+//Ternary Operator Script
+let num11 = -11;
+
+console.log((num11>=0)?"Positive":"Negative")
+
+
+
+
